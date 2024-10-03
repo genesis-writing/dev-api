8e06c69f801920907adc017c897cefa92336e9c6
Keming He <keminghe.career@gmail.com>
2024-10-02 21:04:08 -0400

    feat(research postings): implemented research postings endpoint, full tested, yet to doc
    current research postings endpoint is a wrapper for a more powerful express google cloud function
    from research mentorship program, which unlike cloudflare workers, has access to full node.js env
    and can run @keminghe/osu/async utils. security and rate-limiting all handled by cloud function.

--- v1-undergrad-research-postings/src/index.ts
+++ v1-undergrad-research-postings/src/index.ts
@@ -11,8 +11,15 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-// OSU data async accessor import.
-import { getResearchPostingsAsync } from "@keminghe/osu/async";
+// Research posting schema and type imports.
+import { type ResearchPosting, ResearchPostingSchema } from "@keminghe/osu";
+
+// -----------------------------------------------------------------------------
+// Research postings API response interface.
+interface ResearchPostingsAPIResponseInterface {
+  timestamp: string;
+  postings: ResearchPosting[];
+}
 
 // -----------------------------------------------------------------------------
 // GET api endpoint to return a list of all undergraduate research postings at OSU.
@@ -20,7 +27,34 @@ export default {
   async fetch(request, env, ctx): Promise<Response> {
     // For GET requests, return a JSON object with the list of undergraduate research postings.
     if (request.method === "GET") {
-      return new Response(JSON.stringify(await getResearchPostingsAsync()), {
+      // Fetch the list of undergraduate research postings from public API.
+      const apiResponse: Response = await fetch(
+        "https://us-central1-dev-research-mentorship-dev.cloudfunctions.net/serveLatestResearchPostings",
+        { method: "GET" }
+      );
+
+      // Handle error if the API request fails.
+      if (!apiResponse.ok) {
+        return new Response("Internal server error.", {
+          status: 500,
+          headers: { "Content-Type": "text/plain" },
+        });
+      }
+
+      // Parse the JSON response from the API, 
+      // and extract the list of research postings to respond to the client.
+      const apiResponseJson: ResearchPostingsAPIResponseInterface = await apiResponse.json();
+      const postings: ResearchPosting[] = apiResponseJson["postings"];
+
+      // Validate the postings array against the schema.
+      if (!ResearchPostingSchema.array().safeParse(postings).success) {
+        return new Response("Internal server error.", {
+          status: 500,
+          headers: { "Content-Type": "text/plain" },
+        });
+      }
+
+      return new Response(JSON.stringify(postings), {
         status: 200,
         headers: { "Content-Type": "application/json" },
       });
